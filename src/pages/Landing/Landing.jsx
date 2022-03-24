e2ccc25ecc5544924059ea0b5fc25f9e7d80bd08
Illia Brytun <elbrytun@gmail.com>
2022-03-24 11:55:19 -0400

    change David

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -7,7 +7,7 @@ const Landing = () => {
       <h1>All-The-Things</h1>
       <Link to="/the-manliest-things">Ben's Things</Link><br/>
       <Link to="/the-functional-things">Shahzad's Things</Link><br/>
-      <Link to="/the-well-styled-things">David's Things</Link><br/>
+      <Link to="/the-well-styled-things">Illia's Things</Link><br/>
       <Link to="/the-silly-things">Erin's Things</Link><br/>
     </>
   )
